04151ce6b3324cde14124aeee9d538964daa9394
CrynesSs <56958119+CrynesSs@users.noreply.github.com>
2021-07-19 19:44:52 +0200

    Delete ExampleProjectTesting/src/main directory

--- ExampleProjectTesting/src/main/java/com/example/Recipes/example/IExampleRecipe.java
+++
@@ -1,8 +0,0 @@
-package com.example.Recipes.example;
-
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraftforge.items.wrapper.RecipeWrapper;
-
-public interface IExampleRecipe extends IRecipe<RecipeWrapper> {
-
-}
